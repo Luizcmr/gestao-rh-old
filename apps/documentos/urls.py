665031b45590632956d4a15a63b589a75873c1cd
Ribeiro <luiz.ribeiro@br.crh.com>
2019-05-16 16:57:40 -0300

    'deploy-16052019'

--- apps/documentos/urls.py
+++ apps/documentos/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import DocumentoCreate
+from .views import DocumentoCreate, DocumentoDelete
 
 urlpatterns = [
     #path('documento', DocumentoList.as_view(), name="lista_documentos"),
     path('novo/<int:funcionario_id>/', DocumentoCreate.as_view(), name='create_documento'),
     #path('editar/<int:pk>/', DocumentoEdit.as_view(), name='edit_documento'),
-    #path('excluir/<int:pk>', DocumentoDelete.as_view(), name="deleta_documento"),
+    path('excluir/<int:pk>', DocumentoDelete.as_view(), name="deleta_documento"),
 ]
--- apps/documentos/views.py
+++ apps/documentos/views.py
@@ -1,7 +1,8 @@
-from django.views.generic import CreateView
+from django.http import HttpResponseRedirect
+from django.views.generic import CreateView,DeleteView
+from django.urls import reverse_lazy
 
 from .models import Documento
-
 
 class DocumentoCreate(CreateView):
     model = Documento
@@ -15,3 +16,18 @@ class DocumentoCreate(CreateView):
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
+
+class DocumentoDelete(DeleteView):
+    model = Documento
+    #success_url = reverse_lazy('edit_funcionario', args=[Documento.funcionario])
+
+    def delete(self, request, *args, **kwargs):
+        """
+        Call the delete() method on the fetched object and then redirect to the
+        success URL.
+        """
+        self.object = self.get_object()
+        success_url = reverse_lazy('edit_funcionario', kwargs={'pk': self.object.funcionario_id})
+        self.object.delete()
+
+        return HttpResponseRedirect(success_url)
